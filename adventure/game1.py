9d864154ac7136bd6ad53a235ba0bf38f4b35040
Dave Tucker <dave@xAptive.net>
2019-06-18 16:55:03 -0400

    Catch ctrl-c during game loop, and exit gracefully

--- adventure/game1.py
+++ adventure/game1.py
@@ -87,53 +87,56 @@ showInstructions()
 #loop forever
 while True:
 
-    showStatus()
+    try:
+        showStatus()
 
-    #get the player's next 'move'
-    #.split() breaks it up into an list array
-    #eg typing 'go east' would give the list:
-    #['go','east']
-    move = ''
-    while move == '':  
-        move = input('>')
+        #get the player's next 'move'
+        #.split() breaks it up into an list array
+        #eg typing 'go east' would give the list:
+        #['go','east']
+        move = ''
+        while move == '':  
+            move = input('>')
 
-    move = move.lower().split()
+        move = move.lower().split()
 
-    #if they type 'go' first
-    if move[0] == 'go':
-        #check that they are allowed wherever they want to go
-        if move[1] in rooms[currentRoom]['directions']:
-            #set the current room to the new room
-            currentRoom = rooms[currentRoom]['directions'][move[1]]
-        #there is no door (link) to the new room
-        else:
-            print('You can\'t go that way!')
+        #if they type 'go' first
+        if move[0] == 'go':
+            #check that they are allowed wherever they want to go
+            if move[1] in rooms[currentRoom]['directions']:
+                #set the current room to the new room
+                currentRoom = rooms[currentRoom]['directions'][move[1]]
+            #there is no door (link) to the new room
+            else:
+                print('You can\'t go that way!')
 
-    #if they type 'get' first
-    if move[0] == 'get' :
-        #if the room contains an item, and the item is the one they want to get
-        if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-            #add the item to their inventory
-            inventory += [move[1]]
-            #display a helpful message
-            print(move[1] + ' got!')
-            #delete the item from the room
-            del rooms[currentRoom]['item']
-            #otherwise, if the item isn't there to get
-        else:
-            #tell them they can't get it
-            print('Can\'t get ' + move[1] + '!')
+        #if they type 'get' first
+        if move[0] == 'get' :
+            #if the room contains an item, and the item is the one they want to get
+            if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+                #add the item to their inventory
+                inventory += [move[1]]
+                #display a helpful message
+                print(move[1] + ' got!')
+                #delete the item from the room
+                del rooms[currentRoom]['item']
+                #otherwise, if the item isn't there to get
+            else:
+                #tell them they can't get it
+                print('Can\'t get ' + move[1] + '!')
 
-    #if they want to exit the game
-    if move[0] == 'exit':
+        #if they want to exit the game
+        if move[0] == 'exit':
+            break
+
+        #player loses if they enter a room with a monster
+        if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
+          print('A monster has got you... GAME OVER!')
+          break
+        
+        #player wins if they get to the garden with a key and a pistol
+        if currentRoom == 'Living Room' and 'key' in inventory and 'pistol' in inventory:
+          print('You escaped the house...YOU WIN!')
+          break
+    except KeyboardInterrupt:
         break
-
-    #player loses if they enter a room with a monster
-    if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
-      print('A monster has got you... GAME OVER!')
-      break
-    
-    #player wins if they get to the garden with a key and a pistol
-    if currentRoom == 'Living Room' and 'key' in inventory and 'pistol' in inventory:
-      print('You escaped the house...YOU WIN!')
-      break
